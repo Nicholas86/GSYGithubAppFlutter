ee20b6cf559f13d7ac36d5e45b84d2a840885092
泽娄 <970649831@qq.com>
2019-08-11 17:03:05 +0800

    flutter_github 客户端

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/zelou/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/zelou/Desktop/GSYGithubAppFlutter"
+export "FLUTTER_TARGET=/Users/zelou/Desktop/GSYGithubAppFlutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/zelou/Desktop/flutter/bin/cache/artifacts/engine/ios"
---
+++ lib/common/config/ignoreConfig.dart
@@ -0,0 +1,5 @@
+class NetConfig {
+  static const CLIENT_ID = "62b72f31d2724ce3f46f";
+
+  static const CLIENT_SECRET = "3be5e7fd88719d6608070970c33cbf97a991c1c9";
+}
